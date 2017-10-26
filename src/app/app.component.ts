610fb9e1f4986d86270368338133823f1bf3e86c
Norbert Virth <norbert.virth@gmail.com>
2017-10-26 14:10:38 +0200

    Working solution: Using globals
    * index.html: Added bootstrap dependencies (using CDN, for simplicity)
    * app.component.ts: Demonstrating this solution works. (See comments in the file)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+// Making the global '$' available for TypeScript usage
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit(): void {
+    console.log("Using globals here...");
+    
+    // Here I am able to use the global '$' jquery object
+    // Plus the extension method 'collapse()', which has been
+    // added to the global $ by the bootstrap.js, in index.html
+    //
+    // To see the effect, shrink your browser's width, until the
+    // navbar burget appears. You will see the dropdown menu opened by default.
+    // If you comment this line out, the dropdown menu will be closed by default.
+    //
+    $('.navbar-collapse').collapse('show');
+    //
+    // Now my questions are:
+    // * Is this the inteded way of using such 3rd party libs, which
+    //   have globals, and patches/extensions to other globals?
+  }
 }
