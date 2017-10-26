e485986c01cce9c3935a21c3ff06639c16fb0f7f
Norbert Virth <norbert.virth@gmail.com>
2017-10-26 14:26:08 +0200

    Not working solution: Trying to use TS imports
    app.component.ts contains the relevant code. Please check the comments in it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+// Importing jquery, declaring '$' locally
+//
+import * as $ from 'jquery';
+
+// The next line will cause this error:
+// Uncaught Error: Bootstrap's JavaScript requires jQuery. jQuery must be included before Bootstrap's JavaScript.
+//
+//import 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit(): void {
+    // Here I would like to use the '$' jquery object, extended
+    // with the method 'collapse()', which has should be
+    // added to it by the bootstrap.js, in index.html
+    //
+    // NOTES
+    // * The '$' object is not declared globally here
+    // * This solution does not work :)
+    //
+    $('.navbar-collapse').collapse('show');
+    //
+    // Now my questions are:
+    // * Can I import jquery into $, and then import bootstrap,
+    //   and make it extend the local $ object?
+    // ** I guess this is not possible. If so, what is the
+    //    intended solution for using such kind of 3rd party
+    //    libs, which use globals and extend them?
+  }
 }
